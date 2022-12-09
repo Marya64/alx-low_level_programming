c8c787ef8e93363074da64e20648312506f369d7
Marya64 <njorogemarya304@gmail.com>
2022-12-09 12:56:24 -0800

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,17 +3,16 @@
  * main -entry point
  * Return: Always 0
  */
-
 int main(void)
 {
-	int c;
+int c;
 
-	c = 0;
-	while
-		(c < 10) {
-			putchar(c + 'a');
-			c++;
-		}
-	putchar('\n');
-	return (0);
+c = 0;
+while
+(c < 10) {
+putchar(c + '0');
+c++;
+}
+putchar('\n');
+return (0);
 }
