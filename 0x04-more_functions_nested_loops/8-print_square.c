926a901653ecc878eae4e7d2c996ced7ddfc9814
Marya64 <njorogemarya304@gmail.com>
2023-01-02 17:45:34 -0800

    sunshine

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,13 +7,13 @@
 
 void print_square(int size)
 {
-	int i = 0, j;
+	int r = 0, s;
 
 	if (size > 0)
 	{
-		for (; i < size; i++)
+		for (; r < size; r++)
 		{
-			for (j = 0; j < size; j++)
+			for (s = 0; s < size; s++)
 				_putchar(35);
 			_putchar('\n');
 		}
