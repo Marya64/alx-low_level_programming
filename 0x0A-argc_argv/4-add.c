ddf71e9a760266527998032a05e59a765f7975e4
Marya64 <njorogemarya304@gmail.com>
2023-01-10 13:56:48 -0800

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+/**
+ * main - main funct
+ * @argc: param count
+ * @argv: param poiter
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int q, r, sum = 0;
+
+	for (q = 1; q < argc; q++)
+	{
+		for (q = 0; argv[q][r] != '\0'; r++)
+		{
+			if (!isdigit(argv[q][r]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[q]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
